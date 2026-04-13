7e99a2ed78c8472ff384b6b10c28087158c661a4
RofaidaElwany <your.email@example.com>
2026-04-13 19:21:29 +0200

    add color palett

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary": "#0062a2",
+        primary: "#0062a2",
         "primary-dim": "#00568e",
         "primary-container": "#75b8fd",
         "background": "#f9f9fa",
@@ -21,6 +21,39 @@ module.exports = {
         "on-surface": "#2f3335",
         "on-surface-variant": "#5b6062",
         "outline-variant": "#afb2b5",
+      },
+      fontFamily: {
+        display: ["Manrope", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+
+      fontSize: {
+        // Display
+        "display-lg": [
+          "3.5rem",
+          { lineHeight: "1.1", fontWeight: "700", letterSpacing: "-0.02em" },
+        ],
+        "display-md": ["3rem", { lineHeight: "1.15", fontWeight: "700" }],
+
+        // Headline
+        "headline-lg": ["2rem", { lineHeight: "1.2", fontWeight: "700" }],
+        "headline-md": ["1.75rem", { lineHeight: "1.25", fontWeight: "600" }],
+        "headline-sm": ["1.5rem", { lineHeight: "1.3", fontWeight: "600" }],
+
+        // Title
+        "title-lg": ["1.25rem", { lineHeight: "1.4", fontWeight: "600" }],
+        "title-md": ["1.125rem", { lineHeight: "1.4", fontWeight: "500" }],
+        "title-sm": ["1rem", { lineHeight: "1.5", fontWeight: "500" }],
+
+        // Body
+        "body-lg": ["1rem", { lineHeight: "1.6" }],
+        "body-md": ["0.875rem", { lineHeight: "1.6" }],
+        "body-sm": ["0.8125rem", { lineHeight: "1.5" }],
+
+        // Label
+        "label-lg": ["0.875rem", { lineHeight: "1.4", fontWeight: "500" }],
+        "label-md": ["0.75rem", { lineHeight: "1.4", fontWeight: "500" }],
+        "label-sm": ["0.6875rem", { lineHeight: "1.3", fontWeight: "500" }],
       },
     },
   },
